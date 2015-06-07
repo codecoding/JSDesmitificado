373ca12a2481d4e8df899fae4c67c5e303f87218
Newton_W <roberto.huertas@outlook.com>
2015-06-07 02:33:02 +0200

    done for tonight

--- exercices/09.functions/index.js
+++ exercices/09.functions/index.js
@@ -29,3 +29,58 @@ var f = function foo() {
 };
 typeof foo; // "undefined"
 f(); // "function"
+
+//as value
+var f1 = function () { return 'hi'; };
+var f2 = f1;   //pointer to function
+var f3 = f1(); //hi
+
+
+//factorial -- callee deprecated in strict mode
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments/callee
+var f = function factorial(num) {
+	if (num <= 1) {
+		return 1;
+	} else {
+		return num * factorial(num - 1)
+	}
+}
+
+var f1 = f;
+var f2 = f;
+console.log(f2(5));
+
+f1 = function () { return 0; };
+factorial = f1;
+console.log(f2(5));
+
+//if you try previous with a function declaration won't work!
+
+//this
+var game = 'double dragon';
+var o = { game: 'shinobi' };
+function getGame() {
+	console.log(this.game);
+}
+
+getGame(); //double dragon
+
+o.getGame = getGame;
+o.getGame(); //shinobi
+
+//another example
+function sayNameForAll() {
+	console.log(this.name);
+}
+var person1 = {
+	name: 'Nicholas',
+	sayName: sayNameForAll
+};
+var person2 = {
+	name: 'Greg',
+	sayName: sayNameForAll
+};
+var name = 'Michael';
+person1.sayName(); // outputs 'Nicholas'
+person2.sayName(); // outputs 'Greg'
+sayNameForAll(); // outputs 'Michael'
